b70bb8cfd7b52a3701d12aade38c0090c88bd092
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-06-23 22:33:39 -0300

    Tela de autenticação, criação de sala, roteamento e navegação, contextos e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+import { Home } from "./pages/Home"
+import { NewRoom } from "./pages/NewRoom"
+import { Room } from "./pages/Room"
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
   return (
-    <h1>Hello World!</h1>
-  );
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContextProvider>
+  )
 }
 
-export default App;
+export default App
