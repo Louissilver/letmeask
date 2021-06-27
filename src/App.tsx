006316959e89bad2a03b6f1b8786a6e099358ad4
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-06-27 10:16:59 -0300

    Revert "Tela de autenticação, criação de sala, roteamento e navegação, contextos e hooks"
    This reverts commit b70bb8cfd7b52a3701d12aade38c0090c88bd092.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-
-import { Home } from "./pages/Home"
-import { NewRoom } from "./pages/NewRoom"
-import { Room } from "./pages/Room"
-
-import { AuthContextProvider } from './contexts/AuthContext'
-
 function App() {
   return (
-    <AuthContextProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-        </Switch>
-      </BrowserRouter>
-    </AuthContextProvider>
-  )
+    <h1>Hello World!</h1>
+  );
 }
 
-export default App
+export default App;
